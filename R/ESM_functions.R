7464df9a67d8308e89426d3fbf1384beada8df79
itchyshin <itchyshin@gmail.com>
2019-05-08 09:42:54 +1000

    creating addition variables

--- R/ESM_functions.R
+++ R/ESM_functions.R
@@ -4,19 +4,17 @@
 #'
 #' @param data: data frame 
 #' @param pval: p value
-#' @param N: the number of species (N) and the degrees of freedom df = N - 2
-#' @param n: alternative sample size: the number of species (n) and the degrees of freedom df = N - 2
+#' @param N: sample size (N: the number of species ) and the degrees of freedom df = N - 2
 #'
 #' @return
 #' @export
 #'
 #' @examples
-p_to_Zr <- function(data, pval, N, n) {
+p_to_Zr <- function(data, pval, N) {
   
   # turning them into strings
   pval <- data[[deparse(substitute(pval))]]
   N <- data[[deparse(substitute(N))]]
-  n <- data[[deparse(substitute(n))]]
   
   # getting t values 
   tval<- -qt(pval, N - 2) 
@@ -30,16 +28,8 @@ p_to_Zr <- function(data, pval, N, n) {
   # getting Var(Zr)
   VZr <- 1 / (N - 3)
   
-  # getting ZrA
-  tvalA<- -qt(pval, n - 2) 
-  rvalA <- tval / sqrt((tvalA^2) + (n - 2))
-  ZrA <- 0.5*(log(1 + rvalA) - log(1 - rvalA))
-  
-  # Var(ZrA)
-  VZrA <- 1 / (n - 3)
-  
   # putting all together
-  Zrs <- tibble(rval, Zr, VZr, rvalA, ZrA, VZrA)
+  Zrs <- tibble(rval, Zr, VZr)
   data <- bind_cols(data, Zrs)
 }
 
